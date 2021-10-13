7e4df11c0f14dcafa8811d0998468410b396d626
skipp <chanchanakoedtho@gmail.com>
2021-10-13 22:40:28 +0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "RxStoreKit",
-            dependencies: ["RxSwift", "RxCocoa"]),
+            dependencies: []),
         .testTarget(
             name: "RxStoreKitTests",
             dependencies: ["RxStoreKit"]),
